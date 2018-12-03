31a1666e9d6f714d9cb97468e5d6807727ef7f6e
Kamil Golec <kumalgfilms@gmail.com>
2018-12-03 21:18:48 +0100

    Day3

--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -4,7 +4,7 @@ using AdventOfCode2018.Days;
 namespace AdventOfCode2018 {
     class Program {
         static void Main(string[] args) {
-            Day02.Solve();
+            Day03.Solve();
             Console.ReadKey();
         }
     }
