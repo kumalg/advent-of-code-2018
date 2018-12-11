a9c9f02b3367179e19114784c0f1e1363f7979bf
Kamil Golec <kamil.golec@ceneo.pl>
2018-12-11 11:52:55 +0100

    Day 11

--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -4,7 +4,7 @@ using AdventOfCode2018.Days;
 namespace AdventOfCode2018 {
     class Program {
         static void Main(string[] args) {
-            Day08.Solve();
+            Day11.Solve();
             Console.ReadKey();
         }
     }
