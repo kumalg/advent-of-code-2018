fcbecab3daf731440de67ea27378738ce8d08fe4
Kamil Golec <kumalgfilms@gmail.com>
2018-12-11 23:59:10 +0100

    Day 9

--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -4,7 +4,7 @@ using AdventOfCode2018.Days;
 namespace AdventOfCode2018 {
     class Program {
         static void Main(string[] args) {
-            Day10.Solve();
+            Day09.Solve();
             Console.ReadKey();
         }
     }
